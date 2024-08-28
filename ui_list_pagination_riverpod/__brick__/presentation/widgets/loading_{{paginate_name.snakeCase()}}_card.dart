d14c50e91c111ec49ff60d957a43c33fea09cf62
mobinkarimi2000 <mobin.codingflutter@gmail.com>
2024-08-28 10:00:52 +0330

    adding bloc ui

---
+++ ui_list_pagination_riverpod/__brick__/presentation/widgets/loading_{{paginate_name.snakeCase()}}_card.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Loading{{paginate_name.pascalCase()}}Card extends StatefulWidget {
+  const Loading{{paginate_name.pascalCase()}}Card({
+    super.key,
+  });
+  @override
+  State<Loading{{paginate_name.pascalCase()}}Card> createState() => _Loading{{paginate_name.pascalCase()}}CardState();
+}
+
+class _Loading{{paginate_name.pascalCase()}}CardState extends State<Loading{{paginate_name.pascalCase()}}Card> {
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox();
+  }
+}
